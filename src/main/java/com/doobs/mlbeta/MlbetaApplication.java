c5990cde5dbb969159a43fcc3c7454e464c798fe
Marc Duby <marc_duby@post.harvard.edu>
2019-12-31 20:59:46 -0500

    Start: adding initial project structure

---
+++ src/main/java/com/doobs/mlbeta/MlbetaApplication.java
@@ -0,0 +1,13 @@
+package com.doobs.mlbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doobs/mlbeta/MlbetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doobs.mlbeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
